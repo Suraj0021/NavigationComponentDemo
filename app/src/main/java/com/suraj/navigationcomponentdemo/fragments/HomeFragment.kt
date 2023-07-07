0d69e507c14e35e0779d5977868328ec7a07f163
suraj <surajmargale777@gmail.com>
2023-07-07 11:25:23 +0530

    1.o

---
+++ app/src/main/java/com/suraj/navigationcomponentdemo/fragments/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.suraj.navigationcomponentdemo.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.suraj.navigationcomponentdemo.R
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        
+
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
